460d1e91c590d284f209dc695c6ba912310d8ef0
BinnyK <BinnyK@users.noreply.github.com>
2017-07-22 08:57:01 +1000

    initial commit, dependencies and file structure

---
+++ test/test.spec.js
@@ -0,0 +1,9 @@
+// var assert = require('chai').assert;
+var expect = require('chai').expect;
+// var should = require('chai').should;
+
+describe('Mocha tests', () => {
+  it('should work...', () => {
+    expect('working!').to.equal('working!')
+  });
+})
